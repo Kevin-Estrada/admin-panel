730089bb2f60eae473ab69641f4df25d6a88bdd4
Kevin-Estrada <ke81297@gmail.com>
2021-10-14 14:00:32 -0500

    Added now the orders controllers as well as worked the model to have db relationships

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 // Dashboards Route
@@ -9,3 +10,6 @@ Route::get('/admin', [DashboardController::class, 'index']);
 
 //Products
 Route::resource('/admin/products', ProductController::class);
+
+//Orders
+Route::resource('/admin/orders', OrderController::class);
